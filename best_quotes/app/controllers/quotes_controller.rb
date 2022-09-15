f8163cdcd41f0682712f731e394a345cf5cecceb
Andrew Novoselac <andrew.novoselac@shopify.com>
2022-09-15 08:44:34 -0400

    Use rerun

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "There gis nothing either good or bad!! " + "but thinking makes it so."
+    "There gis nothing either good or bad!!!!! " + "but thinking makes it so."
   end
 
   def exception
