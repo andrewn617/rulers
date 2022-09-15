49e08394f03d0af821d001d306b5fadce8a60bc2
Andrew Novoselac <andrew.novoselac@shopify.com>
2022-09-15 09:24:46 -0400

    Implement a view

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "There gis nothing either good or bad!!!!! " + "but thinking makes it so."
+    render :a_quote, noun: :winking
   end
 
   def exception
